e67a4a0ed0c80e69f3d34678dccc73698d435b45
Jannes Hachmer <jannes.hachmer@gmail.com>
2024-05-06 01:43:21 +0200

    extended tests

--- model/types_test.go
+++ model/types_test.go
@@ -44,3 +44,23 @@ func TestCalcCoeffSum2D(t *testing.T) {
 		t.Errorf("expected %d; got %d", want, got)
   }
 }
+
+func TestCalcMagnitude(t *testing.T) {
+	type test struct {
+		name string
+		args Gradient2D
+		want uint8
+	}
+  tests := []test{
+    {name: "First (1,1)", args: Gradient2D{X: 1 + 127, Y: 1 + 127}, want: 0 },
+    {name: "Second (10,10)", args: Gradient2D{X: 10 + 127, Y: 10 + 127}, want: 1},
+    {name: "Third (100,100)", args: Gradient2D{X: 100 + 127, Y: 100 + 127}, want: 17},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.args.CalcMagnitude(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CalcMagnitude() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
