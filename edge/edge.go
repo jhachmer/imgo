6dc7af65ca8fc5f49722e179981eac3ff2aaea1e
Jannes <jannes.hachmer@gmail.com>
2024-05-12 03:41:32 +0200

    Refactored Sobel Operator

--- edge/edge.go
+++ edge/edge.go
@@ -1,79 +1,99 @@
 package edge
 
 import (
+	"errors"
 	"image"
 
-	m "github.com/jhachmer/gocv/model"
+	"github.com/jhachmer/gocv/model"
+	"github.com/jhachmer/gocv/utils"
 )
+
+var SOBEL_KERNEL_X model.Kernel2D = *model.NewKernel2D([][]int{
+	{-3, 0, 3},
+	{-10, 0, 10},
+	{-3, 0, 3},
+})
+
+var SOBEL_KERNEL_Y model.Kernel2D = *model.NewKernel2D([][]int{
+	{-3, -10, -3},
+	{0, 0, 0},
+	{3, 10, 3},
+})
+var SOBEL_HALF_KERNEL_SIZE = SOBEL_KERNEL_X.XLen / 2
+var SOBEL_COEFF_SUM = 8
 
 // SobelOperator Applies Sobel Kernel to given (grayscale) image
 // Returns 2D-Slice containing Gradient-(Vectors) for each pixel
-func SobelOperator(grayImg *image.Gray) [][]m.Gradient2D {
+func SobelOperator(grayImg *image.Gray) ([][]model.Gradient2D, error) {
 	var (
 		boundsMinX = grayImg.Bounds().Min.X
 		boundsMinY = grayImg.Bounds().Min.Y
 		boundsMaxX = grayImg.Bounds().Max.X
 		boundsMaxY = grayImg.Bounds().Max.Y
 	)
-	kernelX := [][]int{
-		{-1, 0, 1},
-		{-2, 0, 2},
-		{-1, 0, 1},
-	}
-	kernelY := [][]int{
-		{-1, -2, -1},
-		{0, 0, 0},
-		{1, 2, 1},
+
+	kernelX := SOBEL_KERNEL_X
+	kernelY := SOBEL_KERNEL_Y
+
+	// Should never happen, but who knwos?
+	if kernelX.Size != kernelY.Size {
+		return nil, errors.New("x and y kernel sizes dont match")
 	}
 
-	K := len(kernelX[0]) / 2
-	L := len(kernelX) / 2
+	K := kernelX.XLen / 2
+	L := kernelX.YLen / 2
 
-	grad2D := make([][]m.Gradient2D, boundsMaxY)
+	// Allocate 2D Array for Gradient Values
+	grad2D := make([][]model.Gradient2D, boundsMaxY)
 	for i := 0; i < len(grad2D); i++ {
-		grad2D[i] = make([]m.Gradient2D, boundsMaxX)
+		grad2D[i] = make([]model.Gradient2D, boundsMaxX)
 	}
-
-	for v := boundsMinY + L; v < boundsMaxY-L-1; v++ {
-		for u := boundsMinX + K; u < boundsMaxX-K-1; u++ {
-			var sumGradX int = 0
-			var sumGradY int = 0
+	var (
+		sumGradX int
+		sumGradY int
+	)
+	//for v := boundsMinY + L; v < boundsMaxY-L-1; v++ {
+	//	for u := boundsMinX + K; u < boundsMaxX-K-1; u++ {
+	for v := boundsMinY; v < boundsMaxY; v++ {
+		for u := boundsMinX; u < boundsMaxX; u++ {
+			sumGradX = 0
+			sumGradY = 0
 			for j := -L; j <= L; j++ {
 				for i := -K; i <= K; i++ {
-					sourcePix := grayImg.GrayAt(u+i, v+j).Y
+					var sourcePix uint8
+					if u+i < 0 || v+j < 0 || u+i > grayImg.Bounds().Max.X-1 || v+j > grayImg.Bounds().Max.Y-1 {
+						sourcePix = 127
+					} else {
+						sourcePix = grayImg.GrayAt(u+i, v+j).Y
+					}
+
 					// X-Kernel
-					kernX := kernelX[j+L][i+K]
+					kernX := kernelX.Values[j+L][i+K]
 					sumGradX = sumGradX + int(sourcePix)*kernX
 					// Y-Kernel
-					kernY := kernelY[j+L][i+K]
+					kernY := kernelY.Values[j+L][i+K]
 					sumGradY = sumGradY + int(sourcePix)*kernY
 				}
 			}
+			sumGradX /= SOBEL_COEFF_SUM
+			sumGradY /= SOBEL_COEFF_SUM
 			// Adjust to negative number range
 			sumGradX += 127
 			sumGradY += 127
-			//clamp if necessary
-			if sumGradX < 0 {
-				sumGradX = 0
-			}
-			if sumGradX > 255 {
-				sumGradX = 255
-			}
-			if sumGradY < 0 {
-				sumGradY = 0
-			}
-			if sumGradY > 255 {
-				sumGradY = 255
-			}
+			// clamp if necessary
+			sumGradX = utils.ClampPixel(sumGradX, 255, 0)
+			sumGradY = utils.ClampPixel(sumGradY, 255, 0)
+			//fmt.Printf("SumGradX: %v\n", sumGradX)
+			//fmt.Printf("SumGradY: %v\n", sumGradY)
 			grad2D[v][u].X = sumGradX
 			grad2D[v][u].Y = sumGradY
 		}
 	}
 
-	return grad2D
+	return grad2D, nil
 }
 
-func CalcMagnitudeFromGradient(grad [][]m.Gradient2D) [][]uint8 {
+func BuildGradientMagnitudeSlice(grad [][]model.Gradient2D, scale int) [][]uint8 {
 	mag2D := make([][]uint8, len(grad))
 	for i := 0; i < len(mag2D); i++ {
 		mag2D[i] = make([]uint8, len(grad[i]))
@@ -81,7 +101,7 @@ func CalcMagnitudeFromGradient(grad [][]m.Gradient2D) [][]uint8 {
 
 	for v := 0; v < len(grad); v++ {
 		for u := 0; u < len(grad[v]); u++ {
-			mag2D[v][u] = grad[v][u].CalcMagnitude()
+			mag2D[v][u] = grad[v][u].CalcMagnitude(1)
 		}
 	}
 
