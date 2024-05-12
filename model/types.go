df8d434c6d0efdc69d0dbe5162e35377e8d9d181
Jannes <jannes.hachmer@gmail.com>
2024-05-12 03:42:04 +0200

    types

--- model/types.go
+++ model/types.go
@@ -17,28 +17,25 @@ type Imaginary struct {
 type Kernel2D struct {
 	Size   int
 	Values [][]int
+	XLen   int
+	YLen   int
 }
 
 type Kernel1D struct {
 	Size   int
 	Values []int
-}
-
-type Filter1D struct {
-	XKernel Kernel1D
-	YKernel Kernel1D
-}
-
-type Filter2D struct {
-	Kernel Kernel2D
+	Len    int
 }
 
 func (g Gradient2D) CalcMagnitude(scale int) uint8 {
+	// Adjust number range
+	//
 	var gX int = int(g.X - 127)
 	var gY int = int(g.Y - 127)
-	fgX := float64(gX) * (1.0 / float64(scale))
-	fgY := float64(gY) * (1.0 / float64(scale))
-	det := float64((fgX * fgX) + (fgY * fgY))
+	scaledGX := float64(gX) * (1.0 / float64(scale))
+	scaledGY := float64(gY) * (1.0 / float64(scale))
+	det := float64((scaledGX * scaledGX) + (scaledGY * scaledGY))
+
 	return uint8(math.Sqrt(det))
 }
 
@@ -49,6 +46,7 @@ func (k Kernel2D) CalcCoeffSum() int {
 			sum += k.Values[i][j]
 		}
 	}
+
 	return sum
 }
 
@@ -57,6 +55,7 @@ func (k Kernel1D) CalcCoeffSum() int {
 	for i := range k.Values {
 		sum += k.Values[i]
 	}
+
 	return sum
 }
 
@@ -64,6 +63,9 @@ func NewKernel2D(values [][]int) *Kernel2D {
 	k := new(Kernel2D)
 	k.Values = values
 	k.Size = k.CalcCoeffSum()
+	k.XLen = len(values[0])
+	k.YLen = len(values)
+
 	return k
 }
 
@@ -71,5 +73,7 @@ func NewKernel1D(values []int) *Kernel1D {
 	k := new(Kernel1D)
 	k.Values = values
 	k.Size = k.CalcCoeffSum()
+	k.Len = len(values)
+
 	return k
 }
--- model/types_test.go
+++ model/types_test.go
@@ -8,7 +8,7 @@ import (
 func TestNewKernel1D(t *testing.T) {
 	want := Kernel1D{Values: []int{1, 1, 1}, Size: 3}
 	got := NewKernel1D([]int{1, 1, 1})
-	if !reflect.DeepEqual(want.Values, *&got.Values) {
+	if !reflect.DeepEqual(want.Values, got.Values) {
 		t.Errorf("expected %+v; got %+v", want.Values, got.Values)
 	}
 	if want.Size != got.Size {
@@ -17,7 +17,7 @@ func TestNewKernel1D(t *testing.T) {
 }
 
 func TestNewKernel2D(t *testing.T) {
-  want := Kernel2D{Values: [][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}, Size: 9}
+	want := Kernel2D{Values: [][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}, Size: 9}
 	got := NewKernel2D([][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}})
 	if !reflect.DeepEqual(want.Values, got.Values) {
 		t.Errorf("expected %+v; got %+v", want.Values, got.Values)
@@ -37,25 +37,25 @@ func TestCalcCoeffSum1D(t *testing.T) {
 }
 
 func TestCalcCoeffSum2D(t *testing.T) {
-  k := Kernel2D{Values: [][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}, Size: 9}
-  want := k.Size
-  got := k.CalcCoeffSum()
-  if want != got {
+	k := Kernel2D{Values: [][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}, Size: 9}
+	want := k.Size
+	got := k.CalcCoeffSum()
+	if want != got {
 		t.Errorf("expected %d; got %d", want, got)
-  }
+	}
 }
 
 func TestCalcMagnitude(t *testing.T) {
 	type test struct {
-		name string
-		grad Gradient2D
-    scale int
-		want uint8
+		name  string
+		grad  Gradient2D
+		scale int
+		want  uint8
 	}
-  tests := []test{
-    {name: "First (1,1)", grad: Gradient2D{X: 1 + 127, Y: 1 + 127},scale: 8, want: 0 },
-    {name: "Second (10,10)", grad: Gradient2D{X: 10 + 127, Y: 10 + 127}, scale: 8, want: 1},
-    {name: "Third (100,100)", grad: Gradient2D{X: 100 + 127, Y: 100 + 127},scale: 8, want: 17},
+	tests := []test{
+		{name: "First (1,1)", grad: Gradient2D{X: 1 + 127, Y: 1 + 127}, scale: 8, want: 0},
+		{name: "Second (10,10)", grad: Gradient2D{X: 10 + 127, Y: 10 + 127}, scale: 8, want: 1},
+		{name: "Third (100,100)", grad: Gradient2D{X: 100 + 127, Y: 100 + 127}, scale: 8, want: 17},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
