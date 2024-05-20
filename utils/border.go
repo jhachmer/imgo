e2df749a568884151766349173638c46bf8af371
Jannes <jannes.hachmer@gmail.com>
2024-05-20 18:09:58 +0200

    added border handling

---
+++ utils/border.go
@@ -0,0 +1,42 @@
+package utils
+
+func BorderDetection(u, v, i, j, xMax, yMax int) (int, int) {
+	var (
+		newU int = u + i
+		newV int = v + j
+	)
+
+	if newU <= 0 {
+		if newV <= 0 {
+			return -u, -v
+		} else {
+			return -u, v
+		}
+	}
+
+	if newV <= 0 {
+		if newU <= 0 {
+			return -u, -v
+		} else {
+			return u, -v
+		}
+	}
+
+	if newU >= xMax {
+		if newV >= yMax {
+			return xMax - i, yMax - j
+		} else {
+			return xMax - i, v
+		}
+	}
+
+	if newV >= yMax {
+		if newU >= xMax {
+			return xMax - i, yMax - j
+		} else {
+			return u, yMax - j
+		}
+	}
+
+	return u, v
+}
