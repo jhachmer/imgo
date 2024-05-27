58d4d37f88a1a1b1040affe556418bb8103969d9
Jannes <jannes.hachmer@gmail.com>
2024-05-28 01:20:56 +0200

    Tests now crash on unexpected errors

--- model/types_test.go
+++ model/types_test.go
@@ -7,7 +7,10 @@ import (
 
 func TestNewKernel1D(t *testing.T) {
 	want := Kernel1D{Values: []int{1, 1, 1}, Size: 3}
-	got := NewKernel1D([]int{1, 1, 1})
+	got, err := NewKernel1D([]int{1, 1, 1})
+	if err != nil {
+		t.Fatalf("%v+", err)
+	}
 	if !reflect.DeepEqual(want.Values, got.Values) {
 		t.Errorf("expected %+v; got %+v", want.Values, got.Values)
 	}
@@ -25,7 +28,10 @@ func TestNewKernel1DError(t *testing.T) {
 
 func TestNewKernel2D(t *testing.T) {
 	want := Kernel2D{Values: [][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}, Size: 9}
-	got := NewKernel2D([][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}})
+	got, err := NewKernel2D([][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}})
+	if err != nil {
+		t.Fatalf("%v+", err)
+	}
 	if !reflect.DeepEqual(want.Values, got.Values) {
 		t.Errorf("expected %+v; got %+v", want.Values, got.Values)
 	}
@@ -44,36 +50,41 @@ func TestNewKernel2DError(t *testing.T) {
 func TestCalcCoefficientSum1D(t *testing.T) {
 	k := Kernel1D{Values: []int{1, 1, 1}, Size: 3}
 	want := k.Size
-	got := k.CalcCoeffSum()
+	got, err := k.CalcCoefficientSum()
+	if err != nil {
+		t.Fatalf("%v+", err)
+	}
 	if want != got {
 		t.Errorf("expected %d; got %d", want, got)
 	}
 }
 
-func TestCalcCoeffSum2D(t *testing.T) {
+func TestCalcCoefficientSum2D(t *testing.T) {
 	k := Kernel2D{Values: [][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}, Size: 9}
 	want := k.Size
-	got := k.CalcCoeffSum()
+	got, err := k.CalcCoefficientSum()
+	if err != nil {
+		t.Fatal(err)
+	}
 	if want != got {
 		t.Errorf("expected %d; got %d", want, got)
 	}
 }
 
 func TestCalcMagnitude(t *testing.T) {
-	type test struct {
-		name  string
-		grad  Gradient2D
-		scale int
-		want  uint8
-	}
-	tests := []test{
-		{name: "First (1,1)", grad: Gradient2D{X: 1 + 127, Y: 1 + 127}, scale: 8, want: 0},
-		{name: "Second (10,10)", grad: Gradient2D{X: 10 + 127, Y: 10 + 127}, scale: 8, want: 1},
-		{name: "Third (100,100)", grad: Gradient2D{X: 100 + 127, Y: 100 + 127}, scale: 8, want: 17},
+	tests := []struct {
+		name string
+		grad Gradient2D
+		want uint8
+	}{
+		{name: "First (1,1)", grad: Gradient2D{X: 1.0, Y: 1.0}, want: 2},
+		{name: "Second (10,10)", grad: Gradient2D{X: 10, Y: 10}, want: 15},
+		{name: "Third (100,100)", grad: Gradient2D{X: 100, Y: 100}, want: 142},
+		{name: "Fourth (1000,1000)", grad: Gradient2D{X: 1000, Y: 1000}, want: 255},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.grad.CalcMagnitude(tt.scale); !reflect.DeepEqual(got, tt.want) {
+			if got := tt.grad.CalcMagnitude(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("CalcMagnitude() = %v, want %v", got, tt.want)
 			}
 		})
