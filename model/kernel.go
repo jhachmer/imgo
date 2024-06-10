49c84d47a5b932348d94a313eb8cc4818973d8e5
Jannes <jannes.hachmer@gmail.com>
2024-06-10 02:13:16 +0200

    moved kernel types and tests to own files

---
+++ model/kernel.go
@@ -0,0 +1,88 @@
+package model
+
+import (
+	"errors"
+
+	"github.com/jhachmer/imgo/mathutil"
+)
+
+type Kernel2D struct {
+	Size   int
+	Values [][]int
+	Width  int
+	Height int
+}
+
+func NewKernel2D(values [][]int) (*Kernel2D, error) {
+	k := new(Kernel2D)
+	k.Values = values
+	err := k.CalcCoefficientSum()
+	if err != nil {
+		return nil, err
+	}
+	k.Width = len(values[0])
+	k.Height = len(values)
+	if k.Width != k.Height {
+		return nil, errors.New("dimensions of kernel are not symmetrical")
+	}
+
+	return k, nil
+}
+
+func (k *Kernel2D) CalcCoefficientSum() error {
+	var sum int
+	for i := range k.Values {
+		for j := range k.Values[i] {
+			sum += mathutil.Abs(k.Values[i][j])
+		}
+	}
+	if sum == 0 {
+		return errors.New("sum of filter coefficients is zero")
+	}
+	k.Size = sum
+
+	return nil
+}
+
+func (k *Kernel2D) GetHalfKernelSize() (int, int) {
+	K := k.Width / 2
+	L := k.Height / 2
+	return K, L
+}
+
+type Kernel1D struct {
+	Size   int
+	Values []int
+	Len    int
+}
+
+func NewKernel1D(values []int) (*Kernel1D, error) {
+	k := new(Kernel1D)
+	k.Values = values
+	err := k.CalcCoefficientSum()
+	if err != nil {
+		return nil, err
+	}
+	k.Len = len(values)
+
+	return k, nil
+}
+
+func (k *Kernel1D) GetHalfKernelSize() int {
+	return k.Len / 2
+
+}
+
+func (k *Kernel1D) CalcCoefficientSum() error {
+	var sum int
+	for i := range k.Values {
+		sum += mathutil.Abs(k.Values[i])
+	}
+	if sum == 0 {
+		return errors.New("sum of filter coefficients is zero")
+	}
+
+	k.Size = sum
+
+	return nil
+}
--- model/kernel_test.go
+++ model/kernel_test.go
@@ -50,44 +50,24 @@ func TestNewKernel2DError(t *testing.T) {
 func TestCalcCoefficientSum1D(t *testing.T) {
 	k := Kernel1D{Values: []int{1, 1, 1}, Size: 3}
 	want := k.Size
-	got, err := k.CalcCoefficientSum()
+	err := k.CalcCoefficientSum()
 	if err != nil {
 		t.Fatalf("%v+", err)
 	}
-	if want != got {
-		t.Errorf("expected %d; got %d", want, got)
+	if want != k.Size {
+		t.Errorf("expected %d; got %d", want, k.Size)
 	}
 }
 
 func TestCalcCoefficientSum2D(t *testing.T) {
 	k := Kernel2D{Values: [][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}}, Size: 9}
 	want := k.Size
-	got, err := k.CalcCoefficientSum()
+	err := k.CalcCoefficientSum()
 	if err != nil {
 		t.Fatal(err)
 	}
-	if want != got {
-		t.Errorf("expected %d; got %d", want, got)
-	}
-}
-
-func TestCalcMagnitude(t *testing.T) {
-	tests := []struct {
-		name string
-		grad Gradient2D
-		want uint8
-	}{
-		{name: "First (1,1)", grad: Gradient2D{X: 1.0, Y: 1.0}, want: 2},
-		{name: "Second (10,10)", grad: Gradient2D{X: 10, Y: 10}, want: 15},
-		{name: "Third (100,100)", grad: Gradient2D{X: 100, Y: 100}, want: 142},
-		{name: "Fourth (1000,1000)", grad: Gradient2D{X: 1000, Y: 1000}, want: 255},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.grad.CalcMagnitude(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("CalcMagnitude() = %v, want %v", got, tt.want)
-			}
-		})
+	if want != k.Size {
+		t.Errorf("expected %d; got %d", want, k.Size)
 	}
 }
 
@@ -103,8 +83,8 @@ func TestKernel2D_GetHalfKernelSize(t *testing.T) {
 			k: Kernel2D{
 				Size:   9,
 				Values: [][]int{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}},
-				XLen:   3,
-				YLen:   3,
+				Width:  3,
+				Height: 3,
 			},
 			want:  1,
 			want1: 1,
@@ -114,8 +94,8 @@ func TestKernel2D_GetHalfKernelSize(t *testing.T) {
 			k: Kernel2D{
 				Size:   25,
 				Values: [][]int{{1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}, {1, 1, 1, 1, 1}},
-				XLen:   5,
-				YLen:   5,
+				Width:  5,
+				Height: 5,
 			},
 			want:  2,
 			want1: 2,
@@ -125,8 +105,8 @@ func TestKernel2D_GetHalfKernelSize(t *testing.T) {
 			k: Kernel2D{
 				Size:   25,
 				Values: [][]int{{1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1, 1}},
-				XLen:   7,
-				YLen:   7,
+				Width:  7,
+				Height: 7,
 			},
 			want:  3,
 			want1: 3,
