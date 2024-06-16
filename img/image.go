3e8bc37281f63fbbed8e61bad33e805feb4af7d5
Jannes <jannes.hachmer@gmail.com>
2024-06-16 21:41:10 +0200

    refactoring and typo fixes

--- img/image.go
+++ img/image.go
@@ -31,7 +31,7 @@ func ConvertToGrayScale(img image.Image) *image.Gray {
 	return gray
 }
 
-// ReadImageFromPath Reads an image from filesystem
+// ReadImageFromPath reads an image from filesystem
 // Requires filepath from working directory
 // Returns image as pointer to Go-type image.Image
 func ReadImageFromPath(path string) image.Image {
@@ -48,9 +48,9 @@ func ReadImageFromPath(path string) image.Image {
 	return sourceImg
 }
 
-// WriteGrayToFilePNG Writes Go image to filesystem
+// ToPNG  writes Go image to filesystem
 // Creates image at given filepath
-func WriteGrayToFilePNG(outputFileName string, newImage *image.Gray) {
+func ToPNG(outputFileName string, newImage *image.Gray) {
 	f, err := os.Create(outputFileName + ".png")
 	if err != nil {
 		log.Fatal(err)
@@ -61,31 +61,7 @@ func WriteGrayToFilePNG(outputFileName string, newImage *image.Gray) {
 	}
 }
 
-// CreateMagnitudeGrayImageFromGradient Converts 2D-Slice of Gradient Pixels to image of gradient magnitudes
-func CreateMagnitudeGrayImageFromGradient(pixels [][]uint8) *image.Gray {
-	gray := image.NewGray(image.Rect(0, 0, len(pixels[0]), len(pixels)))
-
-	var curMax uint8 = 0
-
-	for j := 0; j <= len(pixels)-1; j++ {
-		subMax := slices.Max(pixels[j])
-		if subMax > curMax {
-			curMax = subMax
-		}
-	}
-
-	var factor float64 = 255.0 / float64(curMax)
-	fmt.Printf("Scale Factor %v\n", factor)
-	for u := 0; u < gray.Bounds().Max.X; u++ {
-		for v := 0; v < gray.Bounds().Max.Y; v++ {
-			magValue := uint8(float64(pixels[v][u]) * factor)
-			gray.SetGray(u, v, color.Gray{Y: magValue})
-		}
-	}
-	return gray
-}
-
-// SliceToImage Converts a 2D-Slice to an image
+// ToImage  converts a 2D-Slice to an image
 // can be saved to file system using decoder
 func ToImage(output Outputer) *image.Gray {
 	var (
