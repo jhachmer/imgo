b2a66bfbd6de0c149091e9fb400e90d567e65cc9
Jannes <jannes.hachmer@gmail.com>
2024-06-20 01:43:15 +0200

    adjusted border detection
    adjusted to now work with 2D-slices instead of raw images

--- border/border.go
+++ border/border.go
@@ -5,33 +5,22 @@ package border
 // (iOffset, jOffset) are the current position in applied filter kernel
 // (xMax, yMax) are the maximum pixel length in each direction
 func Detection(u, v, iOffset, jOffset, xMax, yMax int) (int, int) {
-	var (
-		newU = u + iOffset
-		newV = v + jOffset
-	)
+	newU := u + iOffset
+	newV := v + jOffset
 
+	// Mirror newU
 	if newU < 0 {
-		if newV < 0 {
-			return -newU, -newV
-		} else {
-			return -newU, newV
-		}
+		newU = -newU
+	} else if newU > xMax {
+		newU = 2*xMax - newU + 1
 	}
 
+	// Mirror newV
 	if newV < 0 {
-		return newU, -newV
+		newV = -newV
+	} else if newV > yMax {
+		newV = 2*yMax - newV + 1
 	}
 
-	if newU > xMax {
-		if newV > yMax {
-			return xMax - iOffset, yMax - jOffset
-		} else {
-			return xMax - iOffset, newV
-		}
-	}
-
-	if newV > yMax {
-		return newU, yMax - jOffset
-	}
 	return newU, newV
 }
