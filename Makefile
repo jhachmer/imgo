eeb2ec458d53057eff61d1119ae4ba23936d3bf4
Jannes <jannes.hachmer@gmail.com>
2024-07-01 17:29:57 +0200

    updated build scripts

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-build:
+cli:
 	go build -o bin/imgo cmd/imgo/main.go
+
+server:
+	go build -o bin/imgo-server cmd/imgo_server/main.go
 
 run:
 	go run main.go
--- build.ps1
+++ build.ps1
@@ -1,2 +1,32 @@
-Write-Output "Building imgo.exe at ./bin/"
-$env:GOOS="windows";$env:GOARCH="amd64"; go build -o .\bin\imgo.exe .\cmd\imgo\main.go
+#Write-Output "Building imgo.exe at ./bin/"
+#$env:GOOS="windows";$env:GOARCH="amd64"; go build -o .\bin\imgo.exe .\cmd\imgo\main.go
+
+
+param(
+    [ValidateSet("cli", "api")]
+    [string]$buildType = "cli"
+)
+
+Write-Output "Building $buildType application at ./bin/"
+
+$env:GOOS = "windows"
+$env:GOARCH = "amd64"
+
+switch ($buildType) {
+    "cli" {
+        $outputFile = ".\bin\imgo-cli.exe"
+        $mainFile = ".\cmd\imgo\main.go"
+    }
+    "api" {
+        $outputFile = ".\bin\imgo-api.exe"
+        $mainFile = ".\cmd\imgo_server\main.go"
+    }
+}
+
+go build -o $outputFile $mainFile
+
+if ($LASTEXITCODE -eq 0) {
+    Write-Output "Build successful: $outputFile"
+} else {
+    Write-Output "Build failed"
+}
