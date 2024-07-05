70f9a19a0f40535539dc6b771ad78309cca8f22d
Jannes <jannes.hachmer@gmail.com>
2024-07-05 08:51:46 +0200

    updated build scripts

--- Makefile
+++ Makefile
@@ -1,8 +1,5 @@
-cli:
-	go build -o bin/imgo cmd/imgo/main.go
-
-server:
-	go build -o bin/imgo-server cmd/imgo_server/main.go
+imgo:
+	go build -o bin/imgo main.go
 
 run:
 	go run main.go
--- build.ps1
+++ build.ps1
@@ -1,27 +1,24 @@
-#Write-Output "Building imgo.exe at ./bin/"
-#$env:GOOS="windows";$env:GOARCH="amd64"; go build -o .\bin\imgo.exe .\cmd\imgo\main.go
+# Write-Output "Building imgo.exe at ./bin/"
+# $env:GOOS="windows";$env:GOARCH="amd64"; go build -o .\bin\imgo.exe .\cmd\imgo\main.go
 
-
-param(
-    [ValidateSet("cli", "api")]
-    [string]$buildType = "cli"
-)
-
-Write-Output "Building $buildType application at ./bin/"
+Write-Output "Building imgo application at ./bin/"
 
 $env:GOOS = "windows"
 $env:GOARCH = "amd64"
 
-switch ($buildType) {
-    "cli" {
-        $outputFile = ".\bin\imgo-cli.exe"
-        $mainFile = ".\cmd\imgo\main.go"
-    }
-    "api" {
-        $outputFile = ".\bin\imgo-api.exe"
-        $mainFile = ".\cmd\imgo_server\main.go"
-    }
-}
+$outputFile = ".\bin\imgo.exe"
+$mainFile = ".\main.go"
+
+# switch ($buildType) {
+#     "cli" {
+#         $outputFile = ".\bin\imgo-cli.exe"
+#         $mainFile = ".\cmd\imgo\main.go"
+#     }
+#     "api" {
+#         $outputFile = ".\bin\imgo-api.exe"
+#         $mainFile = ".\cmd\imgo_server\main.go"
+#     }
+# }
 
 go build -o $outputFile $mainFile
 
