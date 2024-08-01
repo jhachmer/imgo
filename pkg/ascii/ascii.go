21c78ebda8e9517c8b7f7ed03068c2875aa70a87
Jannes <jannes.hachmer@gmail.com>
2024-08-01 05:00:50 +0200

    Split ASCII conversion into two functions

--- pkg/ascii/ascii.go
+++ pkg/ascii/ascii.go
@@ -2,38 +2,50 @@ package ascii
 
 import (
 	"fmt"
-	"image/color"
-	"image/png"
 	"log"
 	"os"
+	"strings"
+
+	"github.com/jhachmer/imgo/internal/ops"
 )
 
-// PrintGray prints image to console
-func PrintGray(path string) {
-	catFile, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer catFile.Close()
-
-	img, err := png.Decode(catFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(img)
-
+func ToAscii(in [][]uint8) [][]string {
+	rows, cols := len(in), len(in[0])
+	ret := ops.GenerateSlice[string](cols, rows)
 	levels := []string{" ", "░", "▒", "▓", "█"}
 
-	for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
-		for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
-			c := color.GrayModel.Convert(img.At(x, y)).(color.Gray)
-			level := c.Y / 51
+	for y := 0; y < rows; y++ {
+		for x := 0; x < cols; x++ {
+			c := in[y][x]
+			level := c / 51
 			if level == 5 {
 				level--
 			}
-			fmt.Print(levels[level])
+			ret[y][x] = levels[level]
 		}
-		fmt.Print("\n")
+	}
+	return ret
+}
+
+func WriteAscii(in [][]string, outPath string) {
+	rows, cols := len(in), len(in[0])
+	f, err := os.Create(outPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	for y := 0; y < rows; y++ {
+		var s strings.Builder
+		for x := 0; x < cols; x++ {
+			_, err := fmt.Fprintf(&s, "%v", in[y][x])
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		_, err := fmt.Fprintln(f, s.String())
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
